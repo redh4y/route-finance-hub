6c3d9c4d88deae2ca3de46c297ee4bd2f2ac0da1
Luiz Fernando <luizfernando.etconx@gmail.com>
2026-04-22 13:59:34 -0300

    Forca atualizacao de cache da PWA

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,9 @@ export default defineConfig(({ mode }) => ({
         ],
       },
       workbox: {
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
         navigateFallbackDenylist: [/^\/~oauth/],
